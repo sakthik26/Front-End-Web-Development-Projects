94645bb146c18758bdcfc5a145dd7b213b9f80a9
Sakthi kumar <rsakthikumar26@gmail.com>
2016-04-09 00:57:43 +0530

    Search function complete

--- Wiki viewer/js/script.js
+++ Wiki viewer/js/script.js
@@ -6,16 +6,25 @@ $(function(){
 
 	$("#searchwiki").click(function(){
 
-		$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=IronMan&limit=20&namespace=0&format=json&callback=?",success).fail(error);
+		$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=BatMan&limit=10&namespace=0&format=json&callback=?",success).fail(error);
 	
      function success(data){
+        var searchTerms=[];
 
-       
-     	var htmlData=data[1][1];
-     	alert(htmlData);
-     	alert(JSON.stringify(data));
+     	searchTerms=data[1].slice(); 
+     	searchDescription = data[2].slice();
+     	searchLink = data[3].slice();
+        createblock(searchTerms,searchDescription,searchLink);
      	/*$("#block").append($.parseHTML(htmlData));*/
      }
+
+      function createblock(searchTerms,searchDescription,searchLink){
+        for(var i=0;i<searchTerms.length;i++){
+      	$("#searchresults").append("<a href="+ searchLink[i] + "><div class=block>"+
+      		searchTerms[i] +"<br>" + searchDescription[i] +
+      		"</div></a>");}
+      }
+      
 
       function error() {
       alert("Error retrieving location information. Please reload the page or try again later.");
