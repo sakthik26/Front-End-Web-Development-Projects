57ad1c3184805b403fcc459b3280e844eeff1906
Sakthi kumar <rsakthikumar26@gmail.com>
2016-04-22 02:17:58 +0530

    Twitch

--- Twitch/js/script.js
+++ Twitch/js/script.js
@@ -1,17 +1,44 @@
 
+"use strict";
 $(function(){
 
-   var streamers = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+   var streamers = ["freecodecamp",
+                     "storbeck", 
+                     "terakilobyte",
+                     "habathcx",
+                     "RobotCaleb",
+                     "thomasballinger",
+                     "noobs2ninjas",
+                     "beohoff"];
    
   
-    $.getJSON("https://api.twitch.tv/kraken/streams/test_channel?callback=?",success);
+    $.getJSON("https://api.twitch.tv/kraken/channels/freecodecamp?callback=?",success);
   
 
    function success(data){
     alert(JSON.stringify(data));
     }
 
-    alert("sdfgfdf");
+    //constructing the api call to twitch.tv
+   /* function getTwitchStatus(element){
+    alert(element);
+    $.getJSON("https://api.twitch.tv/kraken/channels/freecodecamp?callback=?",success).fail(error);
+  
+
+     function success(streamersData){
+       alert(JSON.stringify(streamersData));
+      }
+    }
+
+    function error(){
+      alert("The requested data is not available at the moment. Please connect after sometime.")
+    }
+    
+
+    
+    streamers.forEach(getTwitchStatus);    
+*/
+
  })();
 
    
