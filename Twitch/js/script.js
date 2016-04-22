37f72fd0c2ec1f5e6dc7e1b8e1b79f5ba6b637c4
Sakthi kumar <rsakthikumar26@gmail.com>
2016-04-22 12:21:34 +0530

    Twitch

--- Twitch/js/script.js
+++ Twitch/js/script.js
@@ -12,33 +12,65 @@ $(function(){
                      "beohoff"];
    
   
-    $.getJSON("https://api.twitch.tv/kraken/channels/freecodecamp?callback=?",success);
+   /* $.getJSON("https://api.twitch.tv/kraken/streams/comster404?callback=?",success);
   
 
    function success(data){
     alert(JSON.stringify(data));
-    }
+    }*/
 
     //constructing the api call to twitch.tv
-   /* function getTwitchStatus(element){
-    alert(element);
-    $.getJSON("https://api.twitch.tv/kraken/channels/freecodecamp?callback=?",success).fail(error);
-  
+    function getTwitchStatus(channel){
+
+    function buildUrl(type,channelName){
+      $.getJSON("https://api.twitch.tv/kraken/streams/"+channelName+"?callback=?",success).fail(error);
+    }
+
+     buildUrl("streams",channel);     
+    
+    // populate the streamer data using constructor call
+    function StreamData(channelData){
+       this.channelName = channelData.name;       
+       this.channelLang = channelData.language;
+       this.logo = channelData.logo;
+       this.url = channelData.url;
+       this.followers = channelData.followers;
+    }
+
 
      function success(streamersData){
-       alert(JSON.stringify(streamersData));
+        alert("dfgdf");
+        if(streamersData.stream === null){
+        $.getJSON("https://api.twitch.tv/kraken/channels/"+channel+"?callback=?",function(channelData){
+        var channelObj = new StreamData(channelData);   
+        channelObj.channelStatus = "offline";
+        alert(JSON.stringify(channelObj));
+        }).fail(error);
+        insertStream(channelObj);
+       }
+
+
+       else if(streamersData.stream === undefined){
+           var accountBlocked = {status: "No Account Found"};
+           insertStream(accountBlocked);
       }
-    }
 
-    function error(){
-      alert("The requested data is not available at the moment. Please connect after sometime.")
-    }
+       else{
+          var streamObj = new StreamData(streamersData);
+          streamObj.channelStatus = "Online";
+          insertstream(streamObj);
+       }
+
     
-
+    }
+    }
+   function error(){
+          alert("The requested data is not available at the moment. Please try after sometime.");
+     }
     
-    streamers.forEach(getTwitchStatus);    
-*/
+   streamers.forEach(getTwitchStatus);    
 
+   
  })();
 
    
