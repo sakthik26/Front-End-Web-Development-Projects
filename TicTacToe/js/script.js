dea2e2cb905ae7b607f9dc43254a6a938e2463ae
Sakthi kumar <rsakthikumar26@gmail.com>
2016-05-04 19:27:32 +0530

    tictactoe- possiblewins

--- TicTacToe/js/script.js
+++ TicTacToe/js/script.js
@@ -29,17 +29,17 @@ $(function(){
    $(".tile").click(function(){
       $(this).text(input);
       userInput.push($(this).data('value'));
-      isItAWin(input);
+      isItAWin(userInput,input);
       random = systemPlay();
       computerInput.push(random);
-      alert(computerInput);
+      
      // computerInput.push(random);
      $("div[data-value="+random+"]").text(cInput);
       // Mark computer input
       /*computerInput.forEach(function systemInput(val){
         
       })*/
-      /*isItAWin(cInput);*/
+      isItAWin(computerInput,cInput);
 
    })
    var random;
@@ -58,20 +58,23 @@ $(function(){
    
    }
 
-   function isItAWin(input){
+   function isItAWin(arr,input){
          let c;
 
       for(var i=0;i<=7;i++){
           
            c=0;
            if(possibleWins[i].every(function(val)
-            userInput.indexOf(val)>=0 && userInput.length >=3)
+            arr.indexOf(val)>=0 && arr.length >=3)
          ){
             c++;
            }
                  
          if(c === 1){
-            alert("you win");
+             if(arr == userInput)
+            alert("Game over. you win");
+             else
+            alert("Game Over.Computer wins");
            }
       }
       
